d9475d06373f4d980c0b4c77a037a11975563ff7
vmrhomes2026 <manoharraj.vanga@gmail.com>
2026-06-28 11:10:14 +0530

    Add src/App.jsx - Main React component with routing

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import WhatsAppButton from './components/WhatsAppButton';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Portfolio from './pages/Portfolio';
+import Gallery from './pages/Gallery';
+import Testimonials from './pages/Testimonials';
+import Contact from './pages/Contact';
+import ScrollToTop from './components/ScrollToTop';
+
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <Navbar />
+      <main className="min-h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/testimonials" element={<Testimonials />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+      <WhatsAppButton />
+    </Router>
+  );
+}
+
+export default App;
